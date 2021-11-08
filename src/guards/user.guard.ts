95fb017cd17e22efd6053d3de4466c47160b6532
Alenavski <boom.alen.qwerty@gmail.com>
2021-11-08 13:21:35 +0300

    Pushed files

---
+++ src/guards/user.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Roles } from '@models/constants/roles';
+import { UserService } from '@service/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+  constructor(private readonly router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot):  boolean | UrlTree {
+    const userModel = UserService.getUserModel();
+    if (userModel?.role === Roles.User) {
+      return true;
+    }
+    else {
+      return this.router.parseUrl('');
+    }
+  }
+
+}
